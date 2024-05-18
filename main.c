70983007e6b11616fe747cb0202dcb848e5a21c6
grappas <grappastudiols@gmail.com>
2024-05-18 13:44:40 +0200

    pressure and temp

--- main.c
+++ main.c
@@ -1,40 +1,64 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <termios.h>
 #include <cjson/cJSON.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+
+typedef struct {
+  uint8_t _time[20];
+  uint8_t _valid[10];
+  uint8_t _lat1[20];
+  uint8_t _lat2[10];
+  uint8_t _lon1[20];
+  uint8_t _lon2[10];
+  uint8_t _speed[10];
+  uint8_t _orient[10];
+  uint8_t _date[20];
+} RMC_t;
+
+typedef struct {
+  RMC_t gpsData;
+  float temp;
+  float pressure;
+
+} parsedToStruct;
 
 int main() {
-    int fd;
-    char buffer[255];
-    struct termios options;
+  int fd;
+  char buffer[255];
 
-    // Open serial port
-    fd = open("/dev/ttyS0", O_RDWR | O_NOCTTY | O_NDELAY);
-    if (fd == -1) {
-        perror("open");
-        return -1;
-    }
+  // union grabber
 
-    // Set serial port options
-    tcgetattr(fd, &options);
-    cfsetispeed(&options, B9600); // Set baud rate to 9600
-    cfsetospeed(&options, B9600);
-    options.c_cflag |= (CLOCAL | CREAD);
-    tcsetattr(fd, TCSANOW, &options);
+  struct termios options;
 
-    // Read data
-  while(1)
-    { int bytes_read = read(fd, buffer, sizeof(buffer));
+  // Open serial port
+  fd = open("/dev/ttyS0", O_RDWR | O_NOCTTY | O_NDELAY);
+  if (fd == -1) {
+    perror("open");
+    return -1;
+  }
+
+  // Set serial port options
+  tcgetattr(fd, &options);
+  cfsetispeed(&options, B9600); // Set baud rate to 9600
+  cfsetospeed(&options, B9600);
+  options.c_cflag |= (CLOCAL | CREAD);
+  tcsetattr(fd, TCSANOW, &options);
+
+  // Read data
+  while (1) {
+    int bytes_read = read(fd, buffer, sizeof(buffer));
     if (bytes_read > 0) {
-        buffer[bytes_read] = '\0'; // Null terminate the string
-        printf("Received: %s\n", buffer);
-    } }
+      buffer[bytes_read] = '\0'; // Null terminate the string
+      printf("Received: %s\n", buffer);
+    }
+  }
 
-    cJSON *root = cJSON_Parse(buffer);
+  cJSON *root = cJSON_Parse(buffer);
 
-    // Close serial port
-    close(fd);
+  // Close serial port
+  close(fd);
 
-    return 0;
+  return 0;
 }
